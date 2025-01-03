1b3f77c605a13526445138fd21521640f70e313a
Satyam Tiwari <satyamt1612@hotmail.com>
2025-01-04 02:46:46 +0530

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Auth from './components/Auth'
 import Home from './components/Home'
-import { AuthProvider } from './hooks/AuthProvider'
+import { AuthProvider, useAuth } from './hooks/AuthProvider'
 import { Toaster } from 'react-hot-toast'
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/login" element={<Auth />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={
+            <AuthWrapper>
+              <Auth />
+            </AuthWrapper>
+          } />
+          <Route path="/" element={
+            <ProtectedRoute >
+              <Home />
+            </ProtectedRoute>
+          } />
         </Routes>
         <Toaster />
       </AuthProvider>
     </BrowserRouter>
   )
 }
+
+function AuthWrapper({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <Navigate to="/" /> : children;
+}
+
 
 export default App
